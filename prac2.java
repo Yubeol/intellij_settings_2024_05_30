bf911e088f8ac56411ed13e4a7266974de5aa73b
Yubeol <113155432+Yubeol@users.noreply.github.com>
2024-05-30 11:50:44 +0900

    Add files via upload

---
+++ prac2.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class prac2 {
+    public static void main(String[] args) {
+        System.out.println("증가");
+        int k = 1;
+        while (k <= 10) {
+            System.out.println(k);
+            k++;
+        }
+        System.out.println("감소");
+        int i = 10;
+        while (i >= 1) {
+            System.out.println(i);
+            i -= 3;
+        }
+    }
+}
